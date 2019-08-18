e6f002a7b3ca579c47de1930df88103f1ae3f1d4
zvogel <zapvskierdude@gmail.com>
2019-08-18 15:36:50 -0700

    problem_1_1 completed, tested on repl.it

--- CHP1_arrays_strings/Problem_1_1.rs
+++ CHP1_arrays_strings/Problem_1_1.rs
@@ -1,4 +1,16 @@
+use std::collections::HashMap;
 
-fn isUnique(s: &str) -> bool {
+fn is_unique(s: &str) -> bool {
+  let mut count = HashMap::new();
+  for a in s.chars() {
+    if count.contains_key(&a) {return false}
+    else {count.insert(a, 1)};
+  }
+  return true;
+}
 
+fn main() {
+  assert_eq!(is_unique("sup"), true);
+  assert_eq!(is_unique("yoooo"), false);
+  println!("success");
 }
