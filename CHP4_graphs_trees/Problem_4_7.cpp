c8813e6ad39b6727b13efa3817b9e3e971d91512
zapv1348 <zacharyapvogel@gmail.com>
2019-08-18 18:51:56 -0700

    4_7 is done, doing more dp problems

--- CHP4_graphs_trees/Problem_4_7.cpp
+++ CHP4_graphs_trees/Problem_4_7.cpp
@@ -1,2 +1,28 @@
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
 // already done in leetcode
 // My leetcode problems 210 and 207
+// lets do it anBuildOrder
+class BuildOrder {
+public:
+    vector<int> findBuildOrder(vector<pair<int,int>> dependencies, int numClass) {
+        auto visited = vector<bool>(numClass, false);
+        auto ret = vector<int>(numClass, 0);
+        
+        for (auto i : dependencies) {
+            unordered_set<int> current_nodes;
+            current_nodes.insert(i.first);
+            for (auto j : dependencies) {
+                if (i != j && i.second == j.first) {
+                    
+                }
+            }
+        }
+
+        return ret;
+    }
+};
---
+++ CHP8_dynamic_programming/Problem_84.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+
+// Return all subsets of a set
+// same as leetcode 78 and 90, solving one of those is sufficient (solved 78)
